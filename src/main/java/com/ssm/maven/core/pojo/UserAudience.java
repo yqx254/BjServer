67ce6b12d75baf15175173ee5a8593ff3d60af33
yqx254 <ravix254@outlook.com>
2020-06-17 17:21:44 +0800

    user controller AOP

---
+++ src/main/java/com/ssm/maven/core/pojo/UserAudience.java
@@ -0,0 +1,12 @@
+package com.ssm.maven.core.pojo;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author fstar
+ */
+public class UserAudience {
+    private final  static Logger log = Logger.getLogger(UserAudience.class);
+
+
+}
